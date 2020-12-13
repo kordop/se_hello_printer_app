3422c3a7b3897b2159eba59fa74334a6388445b1
kordop <kordop@users.noreply.github.com>
2020-12-13 12:40:49 +0100

    changed from iterator class to generator def

--- test/test_formater.py
+++ test/test_formater.py
@@ -1,32 +1,25 @@
 import unittest
 import random
 import string
+import tracemalloc
+import time
 
 from hello_world.formater import plain_text_upper_case
 
-class StringIterator():
-   '''
-   iterator for creating test string
-   '''
-   def __init__(self, max_str_len: int, number_at_each_len: int, printable: str=string.printable):
-       self._max_str_len = max_str_len
-       self._number_at_each_len = number_at_each_len
-       self._printable = printable
-       self._index = 0
+def string_generator(max_str_len: int, number_at_each_len: int, printable: str=string.printable):
+    '''
+    generator for creating test string
+    '''
 
-   def __next__(self):
-        if self._index > self._number_at_each_len * self._max_str_len:
-            # End of Iteration
-            raise StopIteration
-
+    index = 0
+    while not index > number_at_each_len * max_str_len:
         result = ""
-        if self._index != 0:
-            actual_str_len = (self._index-1) // (self._number_at_each_len)+1
+        if index != 0:
+            actual_str_len = (index-1) // (number_at_each_len)+1
             for _ in range(actual_str_len):
-                result = result + self._printable[random.randint(0,len(self._printable)-1)]
-
-        self._index = self._index + 1
-        return result
+                result = result + printable[random.randint(0,len(printable)-1)]
+        index = index + 1
+        yield(result)
 
 
 class TestFormater(unittest.TestCase):
@@ -38,16 +31,11 @@ class TestFormater(unittest.TestCase):
         self.assertTrue(msg.isupper())
 
     def test_plain_uppercase_str_iter(self):
-        string_iterator = StringIterator(10,10,)
-        while True:
-            try:
-                generated_str = next(string_iterator)
-                r = plain_text_upper_case(generated_str, "EEEMSG")
-                splited = r.split(" ")
-                msg = splited[0]
-                name = splited[-1]
-                self.assertEqual(name, generated_str.upper())
-                self.assertEqual(msg, "EEEMSG".upper())
-            except StopIteration:
-                break
+        for generated_str in string_generator(10,10,"abcdfgHJLASKJKN"):
+            r = plain_text_upper_case(generated_str, "EEEMSG")
+            splited = r.split(" ")
+            msg = splited[0]
+            name = splited[-1]
+            self.assertEqual(name, generated_str.upper())
+            self.assertEqual(msg, "EEEMSG".upper())
 
