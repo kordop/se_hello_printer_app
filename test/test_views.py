4e6d082494636bfac70b72e9bf9fc83b4baaa629
kordop <kordop@users.noreply.github.com>
2020-12-13 10:39:27 +0100

    added missing test of outputs

--- test/test_views.py
+++ test/test_views.py
@@ -1,4 +1,6 @@
+
 import unittest
+
 from hello_world import app
 from hello_world.formater import SUPPORTED
 
@@ -13,6 +15,18 @@ class FlaskrTestCase(unittest.TestCase):
         s = str(rv.data)
         ','.join(SUPPORTED) in s
 
-    def test_msg_with_output(self):
+    def test_msg_with_output_json(self):
         rv = self.app.get('/?output=json')
         self.assertEqual(b'{ "imie":"Natalia", "mgs":Hello World!"}', rv.data)
+
+    def test_msg_with_output_plain(self):
+        rv = self.app.get('/?output=plain')
+        self.assertEqual(b'Natalia Hello World!', rv.data)
+    
+    def test_msg_with_output_upper(self):
+        rv = self.app.get('/?output=plain_uppercase')
+        self.assertEqual(b'NATALIA HELLO WORLD!', rv.data)
+    
+    def test_msg_with_output_lower(self):
+        rv = self.app.get('/?output=plain_lowercase')
+        self.assertEqual(b'natalia hello world!', rv.data)
