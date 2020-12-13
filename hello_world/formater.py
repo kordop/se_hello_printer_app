ba2032526a1f35048d242e1da35cf4b4ad24a56a
kordop <kordop@users.noreply.github.com>
2020-12-13 11:01:18 +0100

    just some fun

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -7,7 +7,7 @@ JSON = "json"
 SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
 
 
-def get_formatted(msg, imie, format):
+def get_formatted(msg: str, imie: str, format: str) -> str:
     result = ""
     if format == PLAIN:
         result = plain_text(msg, imie)
@@ -20,18 +20,20 @@ def get_formatted(msg, imie, format):
     return result
 
 
-def format_to_json(msg, imie):
+def format_to_json(msg: str, imie: str) -> str:
     return ('{ "imie":"' + imie + '", "mgs":' +
             msg + '"}')
 
 
-def plain_text(msg, imie):
+def plain_text(msg: str, imie: str) -> str:
     return imie + ' ' + msg
 
 
-def plain_text_upper_case(msg, imie):
+def plain_text_upper_case(msg: str, imie: str) -> str:
     return plain_text(msg.upper(), imie.upper())
 
 
-def plain_text_lower_case(msg, imie):
+def plain_text_lower_case(msg: str, imie: str) -> str:
     return plain_text(msg.lower(), imie.lower())
+
+print(plain_text_upper_case("","c").split(" "))
